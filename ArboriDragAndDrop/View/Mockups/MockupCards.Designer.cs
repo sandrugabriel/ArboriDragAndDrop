7a8dc8f5cbd795f3e3483e3f498d9c7d17d0cf74
Sandru Gabriel <gabi200611@yahoo.com>
2023-11-11 17:22:11 +0200

    done

--- ArboriDragAndDrop/View/Mockups/MockupCards.Designer.cs
+++ ArboriDragAndDrop/View/Mockups/MockupCards.Designer.cs
@@ -35,11 +35,23 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pct1 = new System.Windows.Forms.PictureBox();
+            this.pct2 = new System.Windows.Forms.PictureBox();
+            this.pct6 = new System.Windows.Forms.PictureBox();
+            this.pct5 = new System.Windows.Forms.PictureBox();
+            this.pct4 = new System.Windows.Forms.PictureBox();
+            this.pct3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel7
             // 
-            this.panel7.Location = new System.Drawing.Point(228, 432);
+            this.panel7.Location = new System.Drawing.Point(241, 432);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(184, 77);
             this.panel7.TabIndex = 1;
@@ -53,43 +65,104 @@
             // 
             // panel5
             // 
-            this.panel5.Location = new System.Drawing.Point(672, 432);
+            this.panel5.Location = new System.Drawing.Point(757, 435);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(184, 77);
             this.panel5.TabIndex = 3;
             // 
             // panel4
             // 
-            this.panel4.Location = new System.Drawing.Point(794, 313);
+            this.panel4.Location = new System.Drawing.Point(859, 326);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(184, 77);
             this.panel4.TabIndex = 4;
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(924, 432);
+            this.panel3.Location = new System.Drawing.Point(976, 435);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(184, 77);
             this.panel3.TabIndex = 5;
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(351, 313);
+            this.panel2.Location = new System.Drawing.Point(354, 326);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(184, 77);
             this.panel2.TabIndex = 6;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(519, 210);
+            this.panel1.Location = new System.Drawing.Point(612, 210);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(289, 77);
+            this.panel1.Size = new System.Drawing.Size(184, 77);
             this.panel1.TabIndex = 7;
+            // 
+            // pct1
+            // 
+            this.pct1.Image = global::ArboriDragAndDrop.Properties.Resources.up_arrow1;
+            this.pct1.Location = new System.Drawing.Point(473, 229);
+            this.pct1.Name = "pct1";
+            this.pct1.Size = new System.Drawing.Size(199, 104);
+            this.pct1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct1.TabIndex = 8;
+            this.pct1.TabStop = false;
+            // 
+            // pct2
+            // 
+            this.pct2.Image = global::ArboriDragAndDrop.Properties.Resources.up_arrow;
+            this.pct2.Location = new System.Drawing.Point(730, 229);
+            this.pct2.Name = "pct2";
+            this.pct2.Size = new System.Drawing.Size(199, 104);
+            this.pct2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct2.TabIndex = 8;
+            this.pct2.TabStop = false;
+            // 
+            // pct6
+            // 
+            this.pct6.Image = global::ArboriDragAndDrop.Properties.Resources.up_arrow;
+            this.pct6.Location = new System.Drawing.Point(976, 388);
+            this.pct6.Name = "pct6";
+            this.pct6.Size = new System.Drawing.Size(109, 53);
+            this.pct6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct6.TabIndex = 8;
+            this.pct6.TabStop = false;
+            // 
+            // pct5
+            // 
+            this.pct5.Image = global::ArboriDragAndDrop.Properties.Resources.up_arrow1;
+            this.pct5.Location = new System.Drawing.Point(842, 388);
+            this.pct5.Name = "pct5";
+            this.pct5.Size = new System.Drawing.Size(109, 53);
+            this.pct5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct5.TabIndex = 8;
+            this.pct5.TabStop = false;
+            // 
+            // pct4
+            // 
+            this.pct4.Image = global::ArboriDragAndDrop.Properties.Resources.up_arrow;
+            this.pct4.Location = new System.Drawing.Point(468, 388);
+            this.pct4.Name = "pct4";
+            this.pct4.Size = new System.Drawing.Size(109, 53);
+            this.pct4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct4.TabIndex = 8;
+            this.pct4.TabStop = false;
+            // 
+            // pct3
+            // 
+            this.pct3.Image = global::ArboriDragAndDrop.Properties.Resources.up_arrow1;
+            this.pct3.Location = new System.Drawing.Point(334, 388);
+            this.pct3.Name = "pct3";
+            this.pct3.Size = new System.Drawing.Size(109, 53);
+            this.pct3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct3.TabIndex = 8;
+            this.pct3.TabStop = false;
             // 
             // MockupCards
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1409, 795);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel6);
@@ -98,11 +171,23 @@
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pct1);
+            this.Controls.Add(this.pct2);
+            this.Controls.Add(this.pct3);
+            this.Controls.Add(this.pct4);
+            this.Controls.Add(this.pct5);
+            this.Controls.Add(this.pct6);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MockupCards";
             this.Text = "MockupCards";
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -116,5 +201,11 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pct2;
+        private System.Windows.Forms.PictureBox pct1;
+        private System.Windows.Forms.PictureBox pct6;
+        private System.Windows.Forms.PictureBox pct5;
+        private System.Windows.Forms.PictureBox pct4;
+        private System.Windows.Forms.PictureBox pct3;
     }
 }
