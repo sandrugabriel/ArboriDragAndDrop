7fd8f950bba8d9d5f1b449991d7aa3fa472fa430
Sandru Gabriel <gabi200611@yahoo.com>
2023-11-11 17:32:01 +0200

    done

--- ArboriDragAndDrop/View/Mockups/MockupCard.Designer.cs
+++ ArboriDragAndDrop/View/Mockups/MockupCard.Designer.cs
@@ -28,24 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(245, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoEllipsis = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ASDASDSADASDASDA";
             // 
             // MockupCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(281, 97);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(184, 77);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MockupCard";
             this.Text = "MockupCard";
@@ -55,6 +56,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
